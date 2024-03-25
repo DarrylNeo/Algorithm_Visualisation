1529fe53a0dcd7ddac253f582cecdf8db0650dac
Sean Huynh <110818854+seangh125@users.noreply.github.com>
2024-03-26 02:44:59 +1100

    Add files via upload

---
+++ backend/Node.py
@@ -0,0 +1,10 @@
+class Node:
+    #initialisation of game state/nodes
+    def __init__(self, state):
+        self.state = state
+        self.visited = 0
+        self.nodeValue = 0
+        self.children = []
+
+    def selectedNode (node):
+        
